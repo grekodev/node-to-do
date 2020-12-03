d0f1827aeb8d20934de0a5c6ef644d4297098bc3
brapastor <brapastor@gmail.com>
2020-12-03 16:19:54 -0500

    firs commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const { argv } = require('./config/yargs');
+const porHacer = require('./por-hacer/por-hacer');
+let commando = argv._[0];
+
+switch (commando) {
+    case 'crear':
+        let tarea = porHacer.crear(argv.descripcion);
+        console.log(tarea);
+        break;
+    case 'listar':
+        let listado = porHacer.getListado();
+        console.log(listado);
+        for (let tarea of listado) {
+            console.log('=============POR HACER================'.green);
+            console.log(tarea.descripcion);
+            console.log('Estado', tarea.completado);
+            console.log('======================================'.green);
+        }
+        break;
+    case 'actualizar':
+        let actualizado = porHacer.actualizar(argv.descripcion, argv.completado);
+        console.log(actualizado);
+        break;
+    case 'borrar':
+        let borrado = porHacer.borrar(argv.descripcion);
+        console.log(borrado);
+        break;
+    default:
+        console.log('Comando no es reconocido')
+
+}
